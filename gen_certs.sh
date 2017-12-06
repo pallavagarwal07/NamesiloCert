823fec691afe4929bb8079a73d348a244a553b93
Pallav Agarwal <pallavagarwal07@gmail.com>
2017-12-06 16:09:37 +0000

    Automatically update certificates in GitLab.

--- gen_certs.sh
+++ gen_certs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # https://stackoverflow.com/questions/59895
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE"  ]; do
@@ -27,3 +29,22 @@ certbot     --config-dir ${DIR}/gen/conf      \
             --noninteractive                  \
             --manual-auth-hook ${DIR}/hook.sh \
             certonly
+
+associations=(
+"pallavagarwal07%2Fshort%2Dlinks pallav.xyz www.pallav.xyz"
+"pallavagarwal07%2Fpallavagarwal07%2Egitlab%2Eio varstack.com www.varstack.com"
+)
+key_dir="${DIR}/gen/conf/live/pallav.xyz"
+for str in "${associations[@]}"; do
+            arr=(${str})
+            project="https://gitlab.com/api/v4/projects/${arr[0]}"
+            domains=("${arr[@]:1}")
+            for d in "${domains[@]}"; do
+                        curl -vvv --header "Private-Token: ${GITLAB}"              \
+                                    --request PUT                                  \
+                                    --form "certificate=@${key_dir}/fullchain.pem" \
+                                    --form "key=@${key_dir}/privkey.pem"           \
+                                    "${project}/pages/domains/${d}" >> ${DIR}/gen/log 2>&1
+            done
+done
+
